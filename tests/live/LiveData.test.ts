bff95aa4cef5853c337c94778e1c42020fc6b6bb
TheyCallMeCheng <cheng-corp@protonmail.com>
2025-11-07 17:51:57 +0100

    Fix: added close connection before each test, unclosed connection would break the tests after the first one

--- tests/live/LiveData.test.ts
+++ tests/live/LiveData.test.ts
@@ -37,66 +37,76 @@ async function getTopMarketsByVolume(quantity:string): Promise<string[]> {
  * Undefined if it fails,
  * Events for the rest
  */
-async function createConnectionWithType(tokenIdsArray:string[], type: string): Promise<BookEvent[]|LastTradePriceEvent[]|TickSizeChangeEvent[]|PriceChangeEvent[]|boolean|undefined> {
+async function createConnectionWithType(tokenIdsArray:string[], type: string):
+Promise<{
+    data: Promise<BookEvent[] | LastTradePriceEvent[] | TickSizeChangeEvent[] | PriceChangeEvent[] | boolean | undefined>,
+    stream: WSSubscriptionManager | undefined} | undefined>
+{
     tokenIdsArray = await getTopMarketsByVolume(marketsQty);
-    let stream:WSSubscriptionManager;
+    let stream: WSSubscriptionManager | undefined;
     try{
         const data = new Promise<BookEvent[]|LastTradePriceEvent[]|TickSizeChangeEvent[]|PriceChangeEvent[]|boolean>((resolve) => {
-        stream = new WSSubscriptionManager({
-            onBook: async (events: BookEvent[]) => {
-                if (type == "onBook") {
-                    resolve(events);
-                }
-            },
-            onLastTradePrice: async (events:LastTradePriceEvent[]) => {
-                if (type == "onLastTradePrice"){
-                    resolve(events)
-                } 
-            },
-            onTickSizeChange: async (events:TickSizeChangeEvent[]) => {
-                if (type == "onTickSizeChange"){
-                    resolve(events)
-                } 
-            },
-            onPriceChange: async (events:PriceChangeEvent[]) => {
-                if (type == "onPriceChange"){
-                    resolve(events)
-                } 
-            },
-            onWSOpen: async () => {
-                if (type == "onWSOpen"){
-                    resolve(true);
-                } 
-            },
-            onError: async (error: Error) => console.error('Error:', error.message)
-        });
-        
-        stream.addSubscriptions(tokenIdsArray);
-        }) 
-        return data;
+            stream = new WSSubscriptionManager({
+                onBook: async (events: BookEvent[]) => {
+                    if (type == "onBook") {
+                        resolve(events);
+                    }
+                },
+                onLastTradePrice: async (events:LastTradePriceEvent[]) => {
+                    if (type == "onLastTradePrice"){
+                        resolve(events)
+                    }
+                },
+                onTickSizeChange: async (events:TickSizeChangeEvent[]) => {
+                    if (type == "onTickSizeChange"){
+                        resolve(events)
+                    }
+                },
+                onPriceChange: async (events:PriceChangeEvent[]) => {
+                    if (type == "onPriceChange"){
+                        resolve(events)
+                    }
+                },
+                onWSOpen: async () => {
+                    if (type == "onWSOpen"){
+                        resolve(true);
+                    }
+                },
+                onError: async (error: Error) => console.error('Error:', error.message)
+            });
+
+            stream.addSubscriptions(tokenIdsArray);
+        })
+        return {data: data, stream: stream};
+
     }catch(e){
         console.log("Error while creating connection: ", e)
+        return undefined;
     }
-    
+
 
 }
 
 describe("onBook", () => {
     let tokenIdsArray;
     let books: any;
+    let stream: WSSubscriptionManager | undefined;
 
     beforeEach(async () => {
         tokenIdsArray = await getTopMarketsByVolume(marketsQty)
-        books = await createConnectionWithType(tokenIdsArray, "onBook")
-    });
+        const result = await createConnectionWithType(tokenIdsArray, "onBook");
+        if (result) {
+            books = await result.data;
+            stream = result.stream;
+        }
+        stream?.clearState()
+    }, 20000);
 
     it('should receive the orderbook', async() => {
         expect(books).toBeDefined()
-        console.log(books)
     }, 5000)
 
     it('should have all fileds', () => {
-        console.log(books)
         books.forEach((book:BookEvent) => {
             expect(book.market).toBeTypeOf('string')
             expect(book.asset_id).toBeTypeOf('string')
@@ -114,4 +124,8 @@ describe("onBook", () => {
         expect(Object.keys(book).sort()).toEqual(expectedKeys.sort());
         });
     });
+})
+
+describe("onLastTradePrice", () => {
+
 })
