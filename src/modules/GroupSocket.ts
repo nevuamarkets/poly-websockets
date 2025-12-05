8fee0f48e22eae650b04fe49f93cf0bf47e8fcd1
Felipe Lima <felipe.lima@gmail.com>
2025-12-04 23:17:01 -0300

    Fix indentation in connect method of GroupSocket

--- src/modules/GroupSocket.ts
+++ src/modules/GroupSocket.ts
@@ -37,9 +37,9 @@ export class GroupSocket {
      * 
      */
     public async connect(): Promise<void> {
-	    if (this.group.connecting) {
-		    return;
-	    }
+        if (this.group.connecting) {
+            return;
+        }
         if (this.group.assetIds.size === 0) {
             this.group.status = WebSocketStatus.CLEANUP;
             return;
@@ -72,8 +72,8 @@ export class GroupSocket {
             this.group.status = WebSocketStatus.DEAD;
             throw err; // caller responsible for error handler
         } finally {
-		    this.group.connecting = false;
-	    }
+            this.group.connecting = false;
+        }
 
         this.setupEventHandlers();
     }
